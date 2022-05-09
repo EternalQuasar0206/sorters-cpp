0e6ee4d390940fce360599d4a22e013755e775e7
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-09 10:09:57 -0300

    chore: quick sort algorithm

--- run.cmd
+++ run.cmd
@@ -7,3 +7,7 @@ g++ bubbleSort.cpp -o .\bin\bubbleSort.exe
 :: Merge sort algorithm
 g++ mergeSort.cpp -o .\bin\mergeSort.exe
 .\bin\mergeSort.exe
+
+:: Quick sort algorithm
+g++ quickSort.cpp -o .\bin\quickSort.exe
+.\bin\quickSort.exe
