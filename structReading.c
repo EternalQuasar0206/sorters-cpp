30ed2d608a1c920add43c468b274e2a62229be5f
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-19 00:56:25 -0300

    Update structReading.c

--- structReading.c
+++ structReading.c
@@ -2,24 +2,41 @@
 #include<string.h>
 
 typedef struct{
-    char name[30];
-    float rc;
-} student;
+    char nome[30];
+    float cr;
+} aluno;
 
 int main() {
     int i;
-    student a[5];
+    aluno a[5];
 
     int index = 0;
 
     while(index < 5) {
-        printf("Type the student name: ");
-        scanf("%s", a[index].name);
+        printf("Digite o nome do aluno: ");
+        scanf("%s", a[index].nome);
 
-        printf("Type the student RC: ");
-        scanf("%e", &(a[index].rc));
+        printf("Digite o CR do aluno: ");
+        scanf("%e", &(a[index].cr));
 
         index++;
+    }
+
+    index = 0;
+    int j;
+    aluno temp;
+
+    for (index = 0; index < 5; ++index)
+    {
+        for (j = index + 1; j < 10; ++j)
+        {
+            if (a[index].cr < a[j].cr)
+            {
+                temp = a[index];
+                a[index] = a[j];
+                a[j] = temp;
+            }
+        }
     }
 
     for(i=0;i<5;i++)
